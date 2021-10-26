b2364d63dbef9890c6f54bb37923ae7a278d5cd0
Hubert <hrozmarynowski776@gmail.com>
2021-10-26 22:50:49 +0200

    Barebones structure

---
+++ daily_tweet.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+
+
+class TwitterBot:
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+    def login(self):
+        pass
+
+    def post(self):
+        pass
+
+    def like_your_own_post(self):
+        pass
+
+    
