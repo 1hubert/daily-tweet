f9dea4a945015e8cadd2b23b4013f5d5aadc610d
Hubert <hrozmarynowski776@gmail.com>
2021-10-27 00:58:32 +0200

    feat: Complete login function

--- daily_tweet.py
+++ daily_tweet.py
@@ -1,18 +1,35 @@
+import time
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class TwitterBot:
     def __init__(self, email, password):
         self.email = email
         self.password = password
+        self.bot = webdriver.Chrome('./resources/chromedriver.exe')
 
     def login(self):
-        pass
+        bot = self.bot
+        bot.get('https://twitter.com')
+        time.sleep(2)
+        email = bot.find_element_by_class_name('email-input')
+        password = bot.find_element_by_name('session[password]')
+        email.clear()  # Just in case
+        password.clear()
+        email.send_keys(self.email)
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+        time.sleep(4)
 
     def post(self):
         pass
 
     def like_your_own_post(self):
         pass
+
+
+jg = TwitterBot('jaroslawgyro4@gmail.com', 'Hub123!@#')
 
     
