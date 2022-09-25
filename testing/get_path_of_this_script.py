7cd9036b166d5c736486103585b3abbcd68c6699
Hubert Rozmarynowski <51206366+1hubert@users.noreply.github.com>
2022-09-25 16:08:34 +0200

    Delete testing directory

--- testing/get_path_of_this_script.py
+++
@@ -1,6 +0,0 @@
-import os
-
-file_path = os.path.realpath(__file__)
-print(os.path.dirname(__file__))
-
-
--- testing/range_testing.py
+++
@@ -1,6 +0,0 @@
-for i in range(1):
-    print(i)
-
-import random
-
-print(random.randint(1,100))
